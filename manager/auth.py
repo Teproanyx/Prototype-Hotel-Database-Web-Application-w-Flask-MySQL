928f20243915a6770dd5a7007978ac5d6763917c
Teproanyx <poomgammer@gmail.com>
2024-05-05 15:32:58 +0700

    Create login logic

--- manager/auth.py
+++ manager/auth.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template
+from flask import Blueprint, request, redirect, url_for, flash, render_template, session
 from mysql.connector import IntegrityError
 
-from werkzeug.security import  generate_password_hash
+from werkzeug.security import  generate_password_hash, check_password_hash
 
 from .db import get_db
 
@@ -50,3 +50,30 @@ def register():
         flash(err)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        db = get_db()
+        error = None
+
+        db.execute(f"SELECT Username, GuestPassword FROM Guest WHERE Username = {username}")
+        user = db.fetchone()
+
+        if user is None:
+            error = "Incorrect username"
+        elif not check_password_hash(user[1], password):
+            error = "Incorrect password"
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user[0]
+            return redirect(url_for('room'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
