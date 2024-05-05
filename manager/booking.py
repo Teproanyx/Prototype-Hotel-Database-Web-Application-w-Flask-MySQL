4773cc2ccf1a487397966c9256a10a8083d493ea
Teproanyx <poomgammer@gmail.com>
2024-05-05 17:43:58 +0700

    Merge my booking with all bookings

--- manager/booking.py
+++ manager/booking.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, redirect, flash, g, render_template, url_for
+from werkzeug.exceptions import abort
 
 from .db import get_db
 from .auth import require_login
@@ -8,7 +9,7 @@ bp = Blueprint("booking", __name__, url_prefix="/booking")
 @bp.route('/index')
 def index():
     db = get_db()
-    db.execute("SELECT RoomNumber, CheckInDate, CheckOutDate FROM Booking")
+    db.execute("SELECT BookingID, RoomNumber, CheckInDate, CheckOutDate, GuestID FROM Booking")
     all_bookings = db.fetchall()
 
     return render_template('booking/index', bookings=all_bookings)
