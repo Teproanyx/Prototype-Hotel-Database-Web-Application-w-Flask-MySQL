3b374c7967f8998f4ea5756a11730eb0cede8cbb
Teproanyx <poomgammer@gmail.com>
2024-05-05 21:03:12 +0700

    Create room view

--- manager/viewing.py
+++ manager/viewing.py
@@ -5,8 +5,17 @@ from .db import get_db
 bp = Blueprint("viewing", __name__)
 
 @bp.route('/')
-def book():
-    pass
+def room():
+    db = get_db()
+    info_header = ("RoomNumber", "RoomName", r"Price/Night" ,"Detail", "Cleaning Staff")
+    
+    db.execute(f'''
+               SELECT RoomNumber, RoomName, PricePerNight, Detail, FirstName
+               FROM Room NATURAL JOIN RoomType NATURAL JOIN Staff
+               ''')
+    
+    return render_template("viewing/viewtable.html", title="Rooms", headers=info_header, 
+                           table=db.fetchall())
 
 
 @bp.route('/staff')
