ad830b56ba15cfc331616fd7acaf93ef010905ed
Teproanyx <poomgammer@gmail.com>
2024-05-05 17:21:00 +0700

    Add index booking page

--- manager/booking.py
+++ manager/booking.py
@@ -4,3 +4,11 @@ from .db import get_db
 from .auth import require_login
 
 bp = Blueprint("booking", __name__, url_prefix="/booking")
+
+@bp.route('/index')
+def index():
+    db = get_db()
+    db.execute("SELECT RoomNumber, CheckInDate, CheckOutDate FROM Booking")
+    all_bookings = db.fetchall()
+
+    return render_template('booking/index', bookings=all_bookings)
