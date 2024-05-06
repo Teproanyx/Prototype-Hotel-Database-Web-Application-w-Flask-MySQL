c536024d051c4b0cba1dddbaf4c4121968cb744e
Teproanyx <poomgammer@gmail.com>
2024-05-06 20:12:55 +0700

    Fix staff view SQL query

--- manager/viewing.py
+++ manager/viewing.py
@@ -7,11 +7,12 @@ bp = Blueprint("viewing", __name__)
 @bp.route('/')
 def room():
     db = get_db()
-    info_header = ("RoomNumber", "RoomName", r"Price/Night", "Details", "Cleaning Staff")
+    info_header = ("Room Number", "Room Type", r"Price/Night", "Capacity", "Details", "Cleaning Staff")
     
-    db.execute(f'''
-               SELECT RoomNumber, RoomName, PricePerNight, Details, FirstName
+    db.execute('''
+               SELECT RoomNumber, RoomName, PricePerNight, Capacity, Details, FirstName
                FROM Room NATURAL JOIN RoomType NATURAL JOIN Staff
+               ORDER BY RoomNumber
                ''')
     
     return render_template("viewing/viewtable.html", title="Rooms", headers=info_header, 
@@ -23,7 +24,7 @@ def staff():
     db = get_db()
     info_header = ("Staff ID", "First Name", "Last Name", "Phone Number", "Email")
     
-    db.execute(f"SELECT Staff ID, First Name, Last Name, Phone, Email FROM Staff")
+    db.execute("SELECT StaffID, FirstName, LastName, Phone, Email FROM Staff")
     
     return render_template("viewing/viewtable.html", title="Staffs", headers=info_header, 
                            table=db.fetchall())
@@ -33,7 +34,7 @@ def caterer():
     db = get_db()
     info_header = ("Caterer ID", "Team Name", "Phone Number", "Email")
 
-    db.execute(f"SELECT CatererID, TeamName, Phone, Email FROM Caterer")
+    db.execute("SELECT CatererID, TeamName, Phone, Email FROM Caterer")
 
     return render_template("viewing/viewtable.html", title="Caterer Teams", headers=info_header,
                            table=db.fetchall())
